51de302f4474dd51ebc66529f081aa2c29399f83
rohan <iamrohanjainn@gmail.com>
2020-10-28 14:33:46 +0530

    added log

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const SeasonDisplay=(props)=>{
-
+    console.log("inside SeasonDisplay")
     console.log(props.lat)
 
     return <div>Season Display :{props.lat}</div>
